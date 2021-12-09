0b2efd3f946ac93f6f3394248a7b602967f7cfbd
Alessandro Sassi <alex.95.saxo@gmail.com>
2021-12-09 20:53:38 +0100

    aggiunta dei file che mancavano

--- middleware/validate-request.js
+++ middleware/validate-request.js
@@ -3,7 +3,7 @@ module.exports = validateRequest;
 function validateRequest(req, next, schema) {
   const options = {
     abortEarly: false, // include all errors
-    allowUnknown: true, // ignore unknown props
+    allowUnknown: true, // ignore unknown prop
     stripUnknown: true // remove unknown props
   };
   const { error, value } = schema.validate(req.body, options);
