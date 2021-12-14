bedf7f2956d128b1440e40edba367dce0a94d1eb
Alessandro Sassi <alex.95.saxo@gmail.com>
2021-12-14 23:54:49 +0100

    ver 0

--- controllers/deleteInvoceController.js
+++ controllers/deleteInvoceController.js
@@ -8,15 +8,21 @@ async function rangeDates (req, res, next) {
         [Sequelize.Op.between]: [req.query.start, req.query.end]
       }}
   })
-
   // console.log(chalk.bgRedBright('query result select bills based on date range'), rangedBills)
   // console.log(chalk.bgRedBright('request'),req.query)
 
   res.json({rangedBills})
 }
 
-function deleteBill () {
-
+function deleteBill (req, res, next) {
+  console.log('request', req)
+  db.Bill.destroy({
+    where:{id_internal_bill: req.body.bill}
+  })
+    .then(numeroRowCancellate =>
+      res.status(200).json({numberDelR:numeroRowCancellate})
+    )
+    .catch(err=>console.log('erroreee', err))
 }
 
 module.exports = {rangeDates, deleteBill}
